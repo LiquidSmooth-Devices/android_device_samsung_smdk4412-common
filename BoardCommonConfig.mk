varund7726/android_device_samsung_smdk4412-common1

smdk4412: Misc cleanup
mcpu, mtune and mfpu should be already added based on board info (like TARGET_CPU_VARIANT and ARCH_ARM_HAVE_NEON)

BoardCommonConfig.mk
@@ -31,12 +31,9 @@ TARGET_CPU_VARIANT := cortex-a9
 ARCH_ARM_HAVE_NEON := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-device_flags := -mtune=cortex-a9 --param l1-cache-size=32 --param l1-cache-line-size=32 --param l2-cache-size=1024
-
-TARGET_GLOBAL_CFLAGS += $(device_flags)
-TARGET_GLOBAL_CPPFLAGS += $(device_flags)
-BOARD_GLOBAL_CFLAGS += $(device_flags)
-BOARD_GLOBAL_CPPFLAGS += $(device_flags)
+# Specify L1/L2 caches used for Exynos 4412
+BOARD_GLOBAL_CFLAGS += --param l1-cache-line-size=32 --param l1-cache-size=32 --param l2-cache-size=1024
+BOARD_GLOBAL_CPPLAGS += --param l1-cache-line-size=32 --param l1-cache-size=32 --param l2-cache-size=1024
 
 EXYNOS4X12_ENHANCEMENTS := true
 EXYNOS4_ENHANCEMENTS := true

LiquidSmooth-Devices/android_device_samsung_smdk4412-common

Conflicts:
	BoardCommonConfig.mk
Change-Id: I217211ad081e8122c09b8d15d8b87efcb3c97165

BoardCommonConfig.mk
@@ -30,8 +30,10 @@ TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_CPU_VARIANT := cortex-a9
 ARCH_ARM_HAVE_NEON := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+
+# Specify L1/L2 caches used for Exynos 4412
+BOARD_GLOBAL_CFLAGS += --param l1-cache-line-size=32 --param l1-cache-size=32 --param l2-cache-size=1024
+BOARD_GLOBAL_CPPFLAGS += --param l1-cache-line-size=32 --param l1-cache-size=32 --param l2-cache-size=1024
 
 EXYNOS4X12_ENHANCEMENTS := true
 EXYNOS4_ENHANCEMENTS := true