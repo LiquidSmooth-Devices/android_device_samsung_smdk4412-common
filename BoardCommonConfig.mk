varund7726/android_device_samsung_smdk4412-common1

smdk4412: Optimizations

BoardCommonConfig.mk
@@ -39,6 +39,10 @@ BOARD_GLOBAL_CPPLAGS += -mtune=cortex-a9
 BOARD_GLOBAL_CFLAGS += --param l1-cache-line-size=32 --param l1-cache-size=32 --param l2-cache-size=1024
 BOARD_GLOBAL_CPPLAGS += --param l1-cache-line-size=32 --param l1-cache-size=32 --param l2-cache-size=1024
 
+# Hint the compiler that we're using quad-core CPU
+BOARD_GLOBAL_CFLAGS += -mvectorize-with-neon-quad
+BOARD_GLOBAL_CPPLAGS += -mvectorize-with-neon-quad
+
 EXYNOS4X12_ENHANCEMENTS := true
 EXYNOS4_ENHANCEMENTS := true
 

LiquidSmooth-Devices/android_device_samsung_smdk4412-common

Change-Id: I132e89e5800382df2bf3bfc3e1172fe189010e75

BoardCommonConfig.mk
@@ -39,6 +39,10 @@ BOARD_GLOBAL_CPPFLAGS += -mtune=cortex-a9
 BOARD_GLOBAL_CFLAGS += --param l1-cache-line-size=32 --param l1-cache-size=32 --param l2-cache-size=1024
 BOARD_GLOBAL_CPPFLAGS += --param l1-cache-line-size=32 --param l1-cache-size=32 --param l2-cache-size=1024
 
+# Hint the compiler that we're using quad-core CPU
+BOARD_GLOBAL_CFLAGS += -mvectorize-with-neon-quad
+BOARD_GLOBAL_CPPFLAGS += -mvectorize-with-neon-quad
+
 EXYNOS4X12_ENHANCEMENTS := true
 EXYNOS4_ENHANCEMENTS := true
 